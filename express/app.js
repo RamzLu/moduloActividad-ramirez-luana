62b71d5ce600d1c2d23527af44abced1be43f4f0
Luana Ramirez <luanaabigail168@gmail.com>
2025-06-06 15:00:46 +0200

    Realicé la funccion para buscar el personaje por id

--- express/app.js
+++ express/app.js
@@ -1,11 +1,26 @@
 import express from 'express'
+import data from "./dataBase.js";
+const items = data.items;
 
+const port = 3001;
 const app = express()
 
 app.get('/api/character/', (req, res) => {
-  res.send('Hello World')
+   res.status(200).json(items);
 })
 
-app.listen(3001, () => {
-    console.log("Servidor corriendo en http://localhost:3001");
-})
+app.get("/api/character/:id", (req, res) => {
+  const { id } = req.params;
+  const personajes = items.find((personaje)=> personaje.id === parseInt(id))
+  if (!personajes){
+    return res.status(404).json({error: 'Personaje de la API no encontrado'})
+  }
+    
+    console.log(`Personaje de ${personajes.name} con ID ${id}`)
+
+    res.status(200).json(personajes);
+});
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
